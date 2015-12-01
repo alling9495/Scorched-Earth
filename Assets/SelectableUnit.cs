2942cdc54876ee48659522acb48557f4dde78135
unknown <alexling@alexling-MOBL.amr.corp.intel.com>
2015-12-01 01:15:32 -0800

    Assets and ProjectSettings added

---
+++ Assets/SelectableUnit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectableUnit : MonoBehaviour {
+
+    private bool selected;
+    public bool Selected
+    {
+        get
+        {
+        }
+        set
+        {
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
